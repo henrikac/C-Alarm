bb68eee3a0aca4e92cac6dd7590aa3f838890a38
Henrik Abel Christensen <sensen1695@hotmail.com>
2018-11-15 20:07:53 +0100

    initial commit

---
+++ src/main.c
@@ -0,0 +1,12 @@
+/*
+ * Project:          C Alarm
+ * Programmer:       Henrik Abel Christensen
+ * Date Completed:   in progress
+*/
+
+#include <stdlib.h>
+
+int main(void)
+{
+  return EXIT_SUCCESS;
+}
