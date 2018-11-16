0358c7fa86f178bd376a3fedbce94ad2efc81d64
Henrik Abel Christensen <sensen1695@hotmail.com>
2018-11-16 07:14:03 +0100

    user can now set new timer

--- src/main.c
+++ src/main.c
@@ -19,37 +19,48 @@
 #endif
 
 #define TIMER_MINUTES 45
+#define MIN_TIMER 1
+#define MAX_TIMER 120
 #define SLEEP 60000
 
 typedef enum { false, true } bool;
 
-void display_main_screen(const char **opt, const size_t num_opts);
+void display_main_screen(const char **opt, const size_t num_opts, const uint8_t timer);
 size_t get_uinput(const char *msg, const uint8_t min_res, const uint8_t max_res);
 void start_timer(const uint8_t minutes, const uint16_t sleep_len);
 void start_alarm();
 
 int main(void)
 {
-  const char *opt[] = {"Start timer", "Exit"};
+  const char *opt[] = {"Start timer", "Set timer", "Exit"};
   size_t num_opts = sizeof(opt) / sizeof(opt[0]);
+  uint8_t timer = TIMER_MINUTES;
   size_t choice;
 
-  display_main_screen(opt, num_opts);
+  display_main_screen(opt, num_opts, timer);
 
-  choice = get_uinput("What to do?", 1, num_opts);
-
-  if (choice == num_opts)
-    exit(EXIT_SUCCESS);
-  else if (choice == 1)
+  while (true)
   {
-    start_timer(TIMER_MINUTES, SLEEP);
-    start_alarm();
+    choice = get_uinput("What to do?", 1, num_opts);
+
+    if (choice == num_opts)
+      exit(EXIT_SUCCESS);
+    else if (choice == 1)
+    {
+      start_timer(timer, SLEEP);
+      start_alarm();
+    }
+    else if (choice == 2)
+    {
+      timer = get_uinput("Enter new timer: ", MIN_TIMER, MAX_TIMER);
+      display_main_screen(opt, num_opts, timer);
+    }
   }
 
   return EXIT_SUCCESS;
 }
 
-void display_main_screen(const char **opt, const size_t num_opts)
+void display_main_screen(const char **opt, const size_t num_opts, const uint8_t timer)
 {
   int i;
 
@@ -58,6 +69,8 @@ void display_main_screen(const char **opt, const size_t num_opts)
   printf("==========\n");
   printf(" C ALARM\n");
   printf("==========\n");
+
+  printf("\n\nCurrent timer: %d %s\n", timer, timer > 1 ? "minutes" : "minute");
 
   for (i = 0; i < num_opts; i++)
   {
@@ -83,7 +96,7 @@ size_t get_uinput(const char *msg, const uint8_t min_res, const uint8_t max_res)
     }
 
     if (res < min_res || res > max_res)
-      printf("\nPlease enter an option between %d and %d", min_res, max_res);
+      printf("\nPlease enter a number between %d and %d", min_res, max_res);
     else
       break;
   }
@@ -95,6 +108,11 @@ void start_timer(const uint8_t minutes, const uint16_t sleep_len)
 {
   int i;
   uint8_t minutes_left;
+
+  system(CLEAR);
+  printf("==============\n");
+  printf(" Countdown\n");
+  printf("==============\n");
 
   for (i = 0; i < minutes; i++)
   {
